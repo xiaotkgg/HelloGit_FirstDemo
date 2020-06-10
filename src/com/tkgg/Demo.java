63fe7e174022a09fd9d2e704bd0a5b6d9d4d9f59
chengkunming <ckm459625@qq.com>
2020-06-10 14:36:24 +0800

    第二次提交文件  修改文件名

--- src/com/tkgg/Demo.java
+++ src/com/tkgg/Demo.java
@@ -5,7 +5,7 @@ package com.tkgg;
  * @Author chengkunming
  * @Date 2020/6/10 0010
  */
-public class demo {
+public class Demo {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
