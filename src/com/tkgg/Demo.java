8f4fc244afc07d3f83cbcdc4f160def8fef24850
tkgg <tkgg12@qq.com>
2020-09-11 09:14:41 +0800

    第二次修改

--- src/com/tkgg/Demo.java
+++ src/com/tkgg/Demo.java
@@ -9,5 +9,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("第一次修改！！！！");
+	System.out.println("第二次修改！！！！！！！");
     }
 }
