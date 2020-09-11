5aac08d23ba9e7e08c77465dd7347cd6428a35dc
tkgg <tkgg12@qq.com>
2020-09-11 15:20:01 +0800

    第一次提交

--- src/com/tkgg/Demo.java
+++ src/com/tkgg/Demo.java
@@ -8,5 +8,6 @@ package com.tkgg;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("小葵花课程开课了！");
+        System.out.println("葵花宝典老是学不好，这是为什么呢？多半是装的，打一顿就能学好了！");
     }
 }
